13abebc67970b12d5c4e578bc352d9890c81595f
Regirug <bubsicle3002@gmail.com>
2018-04-14 17:13:12 +0200

    Getting started
    - Website files made
    - Found colour scheme
    - Found fonts
    - Styled site with placeholder content

---
+++ inc/nav.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono:400,500|Josefin+Sans:400,700|Old+Standard+TT:400,700" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>SOTOMUN</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light">
+  <a class="navbar-brand" href="index.php">SOTOMUN</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="index.php">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="info.php">Info</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="registration.php">Registration</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="conference.php">Conference</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="schedule.php">Schedule</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Team</a>
+      </li>
+    </ul>
+  </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php include("inc/nav.php")?>
+<div class="container">
+  <div class="front">
+    <img class="sotomun-logo" src="assets/sotomun.png">
+    <h1 class="intro-text">Sotogrande International School Model United Nations</h1>
+  </div>
+</div>
+<div class="main">
+  <div class="container">
+    <h2>Secretary-General's welcoming words</h2>
+    <p><span class="illumination">L</span>orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultricies leo integer malesuada nunc vel risus. Massa tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada. Platea dictumst quisque sagittis purus. Amet luctus venenatis lectus magna fringilla. Cursus metus aliquam eleifend mi in nulla. Vulputate ut pharetra sit amet aliquam id diam maecenas ultricies. Adipiscing commodo elit at imperdiet dui. Montes nascetur ridiculus mus mauris vitae ultricies leo integer malesuada. Mollis nunc sed id semper risus in. Eget nunc scelerisque viverra mauris in aliquam. Ac auctor augue mauris augue neque gravida in fermentum et. Platea dictumst quisque sagittis purus sit amet. Urna neque viverra justo nec ultrices dui sapien eget mi. Integer eget aliquet nibh praesent tristique magna sit.
+    </p>
+
+    <p>Lobortis scelerisque fermentum dui faucibus in. Facilisis magna etiam tempor orci eu lobortis elementum nibh. Ac placerat vestibulum lectus mauris. Scelerisque viverra mauris in aliquam sem fringilla ut. Sodales ut etiam sit amet nisl. Augue ut lectus arcu bibendum at. Venenatis tellus in metus vulputate. Augue mauris augue neque gravida in fermentum et sollicitudin. Cras sed felis eget velit aliquet sagittis id consectetur. Justo laoreet sit amet cursus sit amet dictum sit amet. Aliquam ultrices sagittis orci a scelerisque purus semper. Suspendisse potenti nullam ac tortor vitae purus faucibus ornare suspendisse. Lorem ipsum dolor sit amet consectetur adipiscing elit ut. Sed risus pretium quam vulputate dignissim suspendisse in est. Id aliquet risus feugiat in ante metus dictum. Adipiscing elit pellentesque habitant morbi. Nibh sed pulvinar proin gravida hendrerit lectus a. Viverra maecenas accumsan lacus vel facilisis. Rhoncus aenean vel elit scelerisque. Elementum pulvinar etiam non quam lacus suspendisse.
+    </p>
+  </div>
+</div>
+<?php include("inc/footer.php")?>
