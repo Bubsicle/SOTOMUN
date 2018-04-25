4ddb699bf1c58e812f16d5e5c92102f2d5658716
Regirug <bubsicle3002@gmail.com>
2018-04-25 18:50:53 +0200

    Added content to Registration page
    - Added instructions to Registration page
    - Added download links to Registration page
    - Added style for download buttons

--- inc/nav.php
+++ inc/nav.php
@@ -8,6 +8,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono:400,500|Josefin+Sans:400,700|Old+Standard+TT:400,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
 
     <title>SOTOMUN</title>
--- registration.php
+++ registration.php
@@ -1,3 +1,30 @@
 <?php include("inc/nav.php")?>
+<div class="main-nobg">
+  <div class="container">
+    <h2>Registration Procedure</h2>
+    <p>Download form <span class="bold">Registration Part 1,</span> then after completion by Responsible teacher for your group, please scan and email to <a href="mailto:sotomun@sis.gl?Subject=Registration"> sotomun@sis.gl</a>  You should receive an acknowledgement email within 24 hours.
+If you have any questions at this stage, please email <a href="mailto:sotomun@sis.gl?Subject=Questions"> sotomun@sis.gl</a> .
+<br><span class="bold"> Closing date for applications is Sunday 20th May 2018.</span></p>
 
+    <p>We hope to be able to accept as many delegates from each school as wish to come, however in the interest of fairness to all schools who apply, we will not guarantee delegate numbers till after closing date. <span class="bold">Definite numbers and country/ committee allocations will be confirmed by Thursday 24th May.</span>
+    <br>(As stated in general information letter, we have the possibility to add more committees if numbers exceed expectations.)
+    </p>
+
+    <p><span class="bold">Registration Part 2</span> form should be completed by <span class="bold">Friday 1 June</span> and should be accompanied by a deposit of 150€ per school. Invoices will then be issued and <span class="bold">full payment of the balance is required by 30th June.</span><br>
+    Note. SotoMUN payment details will be shared with participating schools in due course.
+    </p>
+  </div>
+</div>
+
+<div class="main">
+  <div class="container">
+    <h2>Downloads</h2>
+    <div class="download" onclick="location.href='assets/registration/RegistrationPart1.docx';">
+      <a href="assets/registration/RegistrationPart1.docx" download>Registration Part 1 <i class="fas fa-download"></i></a>
+    </div>
+    <div class="download" onclick="location.href='assets/registration/RegistrationPart2.docx';">
+      <a href="assets/registration/RegistrationPart2.docx" download>Registration Part 2 <i class="fas fa-download"></i></a>
+    </div>
+  </div>
+</div>
 <?php include("inc/footer.php")?>
