b05f3e9f8f528628cad3cfbd40c0b4d3967cf3b3
Regirug <bubsicle3002@gmail.com>
2018-05-06 20:36:11 +0200

    Alt Text
    - Added alt text to team images

--- team.php
+++ team.php
@@ -3,14 +3,14 @@
 <div class="container">
   <div class="card-deck mt-4">
   <div class="card">
-    <img class="card-img-top" src="assets/team/alexandraeleferenko.jpg" alt="Card image cap">
+    <img class="card-img-top" src="assets/team/alexandraeleferenko.jpg" alt="Picture of Alexandra Eleferenko">
     <div class="card-body">
       <h5 class="card-title">Alexandra Eleferenko</h5>
       <p class="card-text">Conference Organiser</p>
     </div>
   </div>
   <div class="card">
-    <img class="card-img-top" src="assets/team/thomasdelathauwer.jpg" alt="Card image cap">
+    <img class="card-img-top" src="assets/team/thomasdelathauwer.jpg" alt="Picture of Thomas De Lathauwer">
     <div class="card-body">
       <h5 class="card-title">Thomas De Lathauwer</h5>
       <p class="card-text">Conference Organiser</p>
@@ -20,21 +20,21 @@
 
 <div class="card-deck mt-4">
 <div class="card">
-  <img class="card-img-top" src="assets/team/aaronsoo.jpg" alt="Card image cap">
+  <img class="card-img-top" src="assets/team/aaronsoo.jpg" alt="Picture of Aaron Soo">
   <div class="card-body">
     <h5 class="card-title">Aaron Soo</h5>
     <p class="card-text">Secretary General</p>
   </div>
 </div>
 <div class="card">
-  <img class="card-img-top" src="assets/team/othmanmouhajir.jpg" alt="Card image cap">
+  <img class="card-img-top" src="assets/team/othmanmouhajir.jpg" alt="Picture of Othman Mouhajir">
   <div class="card-body">
     <h5 class="card-title">Othman Mouhajir​​</h5>
     <p class="card-text">Deputy Secreatry General</p>
   </div>
 </div>
 <div class="card">
-  <img class="card-img-top" src="assets/team/cedricsanceau.jpg" alt="Card image cap">
+  <img class="card-img-top" src="assets/team/cedricsanceau.jpg" alt="Picture of Cedric Sanceau">
   <div class="card-body">
     <h5 class="card-title">Cedric Sanceau​​</h5>
     <p class="card-text">President of the GA</p>
@@ -44,19 +44,19 @@
 <h2 class="mt-4">Secretariat</h2>
 <div class="card-deck">
 <div class="card">
-  <img class="card-img-top" src="assets/team/victoriavalero.jpg" alt="Card image cap">
+  <img class="card-img-top" src="assets/team/victoriavalero.jpg" alt="Picture of Victoria Valero">
   <div class="card-body">
     <h5 class="card-title">Victoria Valero</h5>
   </div>
 </div>
 <div class="card">
-  <img class="card-img-top" src="assets/team/laurapastor.jpg" alt="Card image cap">
+  <img class="card-img-top" src="assets/team/laurapastor.jpg" alt="Picture of Laura Pastor">
   <div class="card-body">
     <h5 class="card-title">Laura Pastor​​</h5>
   </div>
 </div>
 <div class="card">
-  <img class="card-img-top" src="assets/team/inesmico.jpg" alt="Card image cap">
+  <img class="card-img-top" src="assets/team/inesmico.jpg" alt="Picture of Ines Mico">
   <div class="card-body">
     <h5 class="card-title">Ines Mico</h5>
   </div>
@@ -65,19 +65,19 @@
 
 <div class="card-deck mt-4">
 <div class="card">
-  <img class="card-img-top" src="http://via.placeholder.com/2880x4320" alt="Card image cap">
+  <img class="card-img-top" src="http://via.placeholder.com/2880x4320" alt="Picture of Paloma Jaudel">
   <div class="card-body">
     <h5 class="card-title">Paloma Jaudel</h5>
   </div>
 </div>
 <div class="card">
-  <img class="card-img-top" src="assets/team/eduardoparra.jpg" alt="Card image cap">
+  <img class="card-img-top" src="assets/team/eduardoparra.jpg" alt="Picture of Eduardo Parra">
   <div class="card-body">
     <h5 class="card-title">Eduardo Parra​​</h5>
   </div>
 </div>
 <div class="card">
-  <img class="card-img-top" src="assets/team/shakirbahida.jpg" alt="Card image cap">
+  <img class="card-img-top" src="assets/team/shakirbahida.jpg" alt="Picture of Shakir Bahida">
   <div class="card-body">
     <h5 class="card-title">Shakir Bahida</h5>
   </div>
@@ -87,20 +87,20 @@
 <h2 class="mt-4">Administrative Team</h2>
   <div class="card-deck">
   <div class="card">
-    <img class="card-img-top" src="assets/team/oliverhoad.jpg" alt="Card image cap">
+    <img class="card-img-top" src="assets/team/oliverhoad.jpg" alt="Picture of Oliver Hoad">
     <div class="card-body">
       <h5 class="card-title">Oliver Hoad</h5>
       <p class="card-text">Website Designer</p>
     </div>
   </div>
   <div class="card">
-    <img class="card-img-top" src="assets/team/emekaokonkwo.jpg" alt="Card image cap">
+    <img class="card-img-top" src="assets/team/emekaokonkwo.jpg" alt="Picture of Emeka Okonkwo">
     <div class="card-body">
       <h5 class="card-title">Emeka Okonkwo​​</h5>
     </div>
   </div>
   <div class="card">
-    <img class="card-img-top" src="http://via.placeholder.com/2880x4320" alt="Card image cap">
+    <img class="card-img-top" src="http://via.placeholder.com/2880x4320" alt="Picture of Kate Hobins">
     <div class="card-body">
       <h5 class="card-title">Kate Hobins​​</h5>
       <p class="card-text">Ceremony Organiser</p>
