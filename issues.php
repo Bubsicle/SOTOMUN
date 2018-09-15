3b77151647face24c5648541c5e510ef492052fb
Oliver <oliver.hoad2002@gmail.com>
2018-09-15 12:36:10 +0200

    15/09/2018 Update
    - Added updated logos
    - Optimised website for different widths
    - Added links to issues page
    - Adjusted nav bar to work better with less width space

--- issues.php
+++ issues.php
@@ -6,20 +6,20 @@
     <h3>Disarmament and International Security Committee &#40;DISEC&#41;</h3>
     <ul>
       <li>Cyber Security and Protecting against Cyber Warfare</li>
-      <li>Measures to Strengthen International Counter-Terrorism Efforts</li>
+      <li><a href="https://drive.google.com/open?id=1JJLX5zN4srEPhnhlFUzU9H-gXycBa5IB" target="_blank">Measures to Strengthen International Counter-Terrorism Efforts</a></li>
       <li>Combating the Illicit Trade in Small Arms and Light Weapons</li>
     </ul>
     <h3>Commission on the Status of Women &#40;CSW&#41;</h3>
     <ul>
       <li>Promoting the Involvement of Women and Youth in Government</li>
-      <li>Combating Violence against Women Migrant Workers</li>
-      <li>Empowering Women and Girls in Rural Contexts</li>
+      <li><a href="https://drive.google.com/open?id=1h0MwXj5H0QGFc6WcKjYerXYmjyAxRWjM" target="_blank">Combating Violence against Women Migrant Workers</a></li>
+      <li><a href="https://drive.google.com/open?id=1ilXPrK5ZdDL0YYaXFng5DjMo_SN0mi5B" target="_blank">Empowering Women and Girls in Rural Contexts</a></li>
     </ul>
       <h3>United Nations Development Programme &#40;UNDP&#41;</h3>
     <ul>
-      <li>The Role of Access to Clean Water in Eradicating Poverty</li>
-      <li>Sustainable Cities and Transportation: A Bridge to Development</li>
-      <li>Promoting Resilient Communities</li>
+      <li><a href="https://drive.google.com/open?id=1ELhyahUN1Si5hVdrDU0yVZ-f0FVH6ypk" target="_blank">The Role of Access to Clean Water in Eradicating Poverty</a></li>
+      <li><a href="https://drive.google.com/open?id=1Uwu_xnh3xVCXGEsTm-_jhlCEA-EeZfMw" target="_blank">Sustainable Cities and Transportation: A Bridge to Development</a></li>
+      <li><a href="https://drive.google.com/open?id=1RjLWu2d2YxCYLNETDy8FrxL67iyky42s" target="_blank">Promoting Resilient Communities</a></li>
     </ul>
   </div>
 </div>
--- sponsors.php
+++ sponsors.php
@@ -5,19 +5,20 @@
     <h2>Sponsors</h2>
     <div class="row">
       <div class="col-lg-4 col-md-12">
-        <a href="https://www.helicopterossanitarios.com"><img class="my-3 heli" src="https://go4healthtravel.com/sites/default/files/styles/flexslider_full/public/HelicopterosSanitarios_1.png?itok=dK-j3_TI" class="img-fluid" alt="Helicopteros Sanitarios Logo"></a>
+        <a href="https://www.helicopterossanitarios.com"><img class="my-3 heli" src="assets/helicopteros.jpg" class="img-fluid" alt="Helicopteros Sanitarios Logo"></a>
         <h3><a href="https://www.helicopterossanitarios.com">Helicopteros Sanitarios</a></h3>
       </div>
 
       <div class="col-lg-4 col-md-12">
-        <a href="https://www.schellhammerbusinessschool.com"><img class="my-3 business" src="https://www.schellhammerbusinessschool.com/wp-content/uploads/sbs-logo-big.png" class="img-fluid" alt="Schellhammer Business School Logo"></a>
-        <h3><a href="https://www.schellhammerbusinessschool.com">Schellhammer Business School</a></h3>
+        <a href="http://rociomorenotextiles.com/rocio/enter.html"><img class="my-3 rocio" src="assets/moreno.jpg" class="img-fluid" alt="Rocio Moreno Logo"></a>
+        <h3><a href="http://rociomorenotextiles.com/rocio/enter.html">Rocio Moreno</a></h3>
       </div>
 
       <div class="col-lg-4 col-md-12">
-        <a href="http://rociomorenotextiles.com/rocio/enter.html"><img class="my-3 rocio" src="http://rociomorenotextiles.com/rocio/home_files/shapeimage_1.png" class="img-fluid" alt="Rocio Moreno Logo"></a>
-        <h3><a href="http://rociomorenotextiles.com/rocio/enter.html">Rocio Moreno</a></h3>
+        <a href="https://www.schellhammerbusinessschool.com"><img class="my-3 business" src="assets/schlammer.jpg" class="img-fluid" alt="Schellhammer Business School Logo"></a>
+        <h3><a href="https://www.schellhammerbusinessschool.com">Schellhammer Business School</a></h3>
       </div>
+
 
     </div>
   </div>
