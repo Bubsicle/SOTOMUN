6567d61b55777f7c82fded876adfb1b7696f230d
Oliver <oliver.hoad2002@gmail.com>
2018-09-15 17:47:07 +0200

    Side update
    - Added new links to issues page

--- issues.php
+++ issues.php
@@ -5,13 +5,13 @@
     <h2>Committees &amp; Issues</h2>
     <h3>Disarmament and International Security Committee &#40;DISEC&#41;</h3>
     <ul>
-      <li>Cyber Security and Protecting against Cyber Warfare</li>
+      <li><a href="https://drive.google.com/open?id=14gn2S03y8eB9ASTZFa31bDQzGgZvwqaH" target="_blank">Cyber Security and Protecting against Cyber Warfare</a></li>
       <li><a href="https://drive.google.com/open?id=1JJLX5zN4srEPhnhlFUzU9H-gXycBa5IB" target="_blank">Measures to Strengthen International Counter-Terrorism Efforts</a></li>
-      <li>Combating the Illicit Trade in Small Arms and Light Weapons</li>
+      <li><a href="https://drive.google.com/open?id=1akKDP4AkIoqIyhKdAqvdUCFyK6XyPQnb" target="_blank">Combating the Illicit Trade in Small Arms and Light Weapons</a></li>
     </ul>
     <h3>Commission on the Status of Women &#40;CSW&#41;</h3>
     <ul>
-      <li>Promoting the Involvement of Women and Youth in Government</li>
+      <li><a href="https://drive.google.com/open?id=1gQ_ErkrhXjjNUj5TFlcwHfmrheHFTfD9" target="_blank">Promoting the Involvement of Women and Youth in Government</a></li>
       <li><a href="https://drive.google.com/open?id=1h0MwXj5H0QGFc6WcKjYerXYmjyAxRWjM" target="_blank">Combating Violence against Women Migrant Workers</a></li>
       <li><a href="https://drive.google.com/open?id=1ilXPrK5ZdDL0YYaXFng5DjMo_SN0mi5B" target="_blank">Empowering Women and Girls in Rural Contexts</a></li>
     </ul>
